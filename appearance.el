2826493a7d1cd4eb98d6a316f60f25a6365ef383
stuart.dilts <c57s199@bz-cs-cslab18l.msu.montana.edu>
2017-10-25 15:58:52 -0600

    Made different theme load when in terminal

--- appearance.el
+++ appearance.el
@@ -1,28 +1,28 @@
 ;; Stuart Dilts 2017
 
-
-(when (not window-system)
-  (menu-bar-mode -1))
-;;  (disable-theme 'deeper-blue)
-;;  (load-theme 'manoj-dark))
-
-(use-package spaceline
-  :ensure t
-  :config
-  (require 'spaceline-config)
-  (spaceline-compile)
-  (spaceline-emacs-theme)
-  (spaceline-toggle-version-control-on)
-  (setf powerline-default-separator 'arrow))
-
-(use-package material-theme
-  :ensure t
-  :config
-  (load-theme 'material t))
-
-(use-package fancy-battery
-  :config
-  (fancy-battery-mode))
-
-;; ;;disables that pesky tool bar.
+;;disables that pesky tool bar.
 (tool-bar-mode -1)
+
+
+(if (not window-system)
+    (progn
+      (menu-bar-mode -1)
+      (load-theme 'manoj-dark))
+  (progn
+    ;;when in a windowed system, use the fancy themes:
+    (use-package spaceline
+      :ensure t
+      :config
+      (require 'spaceline-config)
+      (spaceline-compile)
+      (spaceline-emacs-theme)
+      (spaceline-toggle-version-control-on)
+      (setf powerline-default-separator 'arrow))
+    (use-package material-theme
+      :ensure t
+      :config
+      (load-theme 'material t))
+
+    (use-package fancy-battery
+      :config
+      (fancy-battery-mode))))
