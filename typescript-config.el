bca109c35243430b4f5367a150eafc38fb06c437
Stuart Dilts <stuart.dilts@mt.gov>
2023-03-31 13:16:58 -0600

    TS: Add use-package tide-mode call
    This isn't the optimal way to code this, but it gets me what I want for
    now. My elisp is rusty, anyhow.

--- typescript-config.el
+++ typescript-config.el
@@ -1,3 +1,6 @@
+(use-package tide-mode
+  :ensure t)
+
 (defun setup-tide-mode ()
   (interactive)
   (tide-setup)
@@ -14,9 +17,39 @@
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 
-(use-package web-mode
-  :defer t
-  :ensure t
-  :config
-  (setq web-mode-engines-alist
-	'(("freemarker" . "\\.ftlh"))))
+;; ng2-mode provides a mode for ts files, but it sucks, so remove it from
+;; (commented out because I'm not using it right now:
+;; (cl-remove-if (lambda (item)
+;; 		(cl-find (car item) '("\\.module.ts\\'"
+;; 				      "\\.guard.ts\\'"
+;; 				      "\\.directive.ts\\'"
+;; 				      "\\.pipe.ts\\'"
+;; 				      "\\.service.ts\\'"
+;; 				      "\\.component.ts\\'"
+;; 				      "\\.component.html\\'")
+;; 			 :test #'string-equal))
+;; 	      auto-mode-alist)
+
+;; (use-package web-mode
+;;   :defer t
+;;   :ensure t
+;;   :config
+;;   (setq web-mode-engines-alist
+;; 	'(("freemarker" . "\\.ftlh"))))
+
+(define-derived-mode ng-template-mode mhtml-mode "Angular Template"
+  "Major mode for Angular template files")
+
+(add-hook 'ng-template-mode-hook #'eglot-ensure)
+
+(add-to-list 'auto-mode-alist '("\\.component.html\\'" . ng-template-mode))
+
+(require 'eglot)
+(add-to-list `eglot-server-programs
+	     `(ng-template-mode "node"
+				"C:/Users/cnc030/node_modules/@angular/language-server/index.js"
+				"--ngProbeLocations"
+				"C:/Users/cnc030/node_modules/"
+				"--tsProbeLocations"
+				"C:/Users/cnc030/node_modules/"
+				"--stdio"))
